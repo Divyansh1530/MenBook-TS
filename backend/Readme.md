57478634c6ccc41c2cbf303177c135de8a35bc96
Divyansh <divyanshyadav1530@gmail.com>
2026-06-13 19:03:09 +0530

    Initialized the backend setup , connected the database

---
+++ backend/Readme.md
@@ -0,0 +1,5 @@
+# Mentor Booking Platform
+
+A Mentor Booking Platform in which user can book mentor
+
+- [Model link](https://app.eraser.io/workspace/wt7YGkmdLGnjLg4enOzt?origin=share&diagram=mTs3fc9fwGOcEFx3zb_PC)
---
+++ backend/src/db/index.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+import {DB_NAME} from '../constants.js'
+
+const connectDB = async():Promise<void> => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        console.log(
+            `\n MongoDB connected successfully, DB HOST:${connectionInstance.connection.host}`
+        )
+
+    } catch (error:unknown) {
+        if (error instanceof Error) {
+            console.log("MongoDB connection failed",error.message)
+        }
+    }
+    process.exit(1)
+}
+
+export default connectDB
