ee525ce1e7e0b10649186e842d213ecd250c1e48
Divyansh <divyanshyadav1530@gmail.com>
2026-07-11 22:56:23 +0530

    same site none

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -185,7 +185,8 @@ const loginUser = asyncHandler(async(req,res) => {
 
     const options:CookieOptions = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite:"none"
     }
 
     return res
